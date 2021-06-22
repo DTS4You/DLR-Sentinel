2985733d2f5c66ce0e62c0a84984459cab945ea2
Norbert Schwarz <info@datentechnikschwarz.de>
2021-06-22 22:21:52 +0200

    init

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    print("Hello World!")
+from machine import Pin, Timer
+led = Pin(25, Pin.OUT)
+timer = Timer()
 
+def blink(timer):
+    led.toggle()
+
+
+def main():
+    print("--- Programm Start ---")
+
+#------------------------------------------------------------------------------
 if __name__ == "__main__":
+
+    timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
+
     main()
+
+#------------------------------------------------------------------------------

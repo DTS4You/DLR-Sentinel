cb0d4fbb635ddfbb006cc4c165cffdd9329bbac7
Norbert.Schwarz <info@datentechnikschwarz.de>
2021-06-23 20:40:03 +0200

    init

---
+++ inc/main_save.py
@@ -0,0 +1,19 @@
+from machine import Pin, Timer
+led = Pin(25, Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle()
+
+
+def main():
+    print("--- Programm Start ---")
+
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    timer.init(freq=1, mode=Timer.PERIODIC, callback=blink)
+
+    main()
+
+#------------------------------------------------------------------------------
--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from machine import Pin, Timer
-led = Pin(25, Pin.OUT)
-timer = Timer()
+import time
+from neopixel import Neopixel
 
-def blink(timer):
-    led.toggle()
+numpix = 16
+strip_1 = Neopixel(numpix, 0, 2, "GRB")
 
+red = (255, 0, 0)
+orange = (255, 165, 0)
+yellow = (255, 150, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+indigo = (75, 0, 130)
+violet = (138, 43, 226)
+white = (200,200,200)
+colors_rgb = (red, white, red, green, blue, indigo, violet)
 
-def main():
-    print("--- Programm Start ---")
+# same colors as normaln rgb, just 0 added at the end
+colors_rgbw = [color+tuple([0]) for color in colors_rgb]
+colors_rgbw.append((0, 0, 0, 255))
 
-#------------------------------------------------------------------------------
-if __name__ == "__main__":
+# uncomment colors_rgb if you have RGB strip
+# colors = colors_rgb
+colors = colors_rgbw
 
-    timer.init(freq=5, mode=Timer.PERIODIC, callback=blink)
+strip_1.brightness(42)
 
-    main()
-
-#------------------------------------------------------------------------------
+while True:
+    for color in colors:
+        for i in range(numpix):
+            strip_1.set_pixel(i, color)
+            time.sleep(0.1)
+            strip_1.show()

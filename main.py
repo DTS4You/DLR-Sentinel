3d5d50f92679abf820e4c844e407325dff5a67de
Norbert.Schwarz <info@datentechnikschwarz.de>
2021-07-05 18:17:15 +0200

    init

--- main.py
+++ main.py
@@ -1,45 +1,18 @@
-import time
-from neopixel import Neopixel
+# Scanner i2c en MicroPython | MicroPython i2c scanner
 
-# Number of LEDs
-numpix = 16
-strip_1 = Neopixel(numpix, 0, 2, "GRB")
-strip_2 = Neopixel(numpix, 1, 3, "GRB")
+import machine
+i2c = machine.I2C(0, scl=machine.Pin(21), sda=machine.Pin(20))
 
-red = (255, 0, 0)
-orange = (255, 165, 0)
-yellow = (255, 150, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
-indigo = (75, 0, 130)
-violet = (138, 43, 226)
-white = (255,255,255)
-grey = (50,50,50)
-black = (0,0,0)
-colors_rgb = (red, white, red, green, blue, indigo, violet)
+print("--------- Start of Program ---------")
+print('Scan i2c bus...')
+devices = i2c.scan()
 
-# same colors as normaln rgb, just 0 added at the end
-colors_rgbw = [color+tuple([0]) for color in colors_rgb]
-colors_rgbw.append((0, 0, 0, 255))
+if len(devices) == 0:
+    print("No i2c device !")
+else:
+    print('i2c devices found:',len(devices))
 
-# uncomment colors_rgb if you have RGB strip
-# colors = colors_rgb
-colors = colors_rgbw
+    for device in devices:  
+        print("Decimal address: ",device," | Hexa address: ",hex(device))
 
-strip_1.brightness(20)
-strip_2.brightness(50)
-
-strip_2.set_pixel_line_gradient(0, 7, white, grey)
-strip_2.set_pixel_line_gradient(8, 15, grey, white)
-
-
-while True:
-    for color in colors:
-        for i in range(numpix):
-            strip_1.set_pixel(i, color)
-            strip_2.rotate_right(1)
-            time.sleep(0.05)
-            strip_1.show()
-            strip_2.show()
-
-# End
+print("---------- End of Program ----------")
